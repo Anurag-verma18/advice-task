80e1f81442d2a8f97c5df73f98a5ac4aaf43bdfe
Anurag-verma18 <anuragkrverma18@gmail.com>
2024-10-22 17:11:05 +0530

    feat: update the card after making api call on each click

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+const adviceData = document.getElementById("advice-data");
+const idVal = document.getElementById("id");
+const diceContainer = document.querySelector(".dice-container")
 
+let value;
 const getApiData = async () => {
-    const data = await fetch("https://api.adviceslip.com");
-    const result = await data.json();
-    console.log(result);
+    try {
+        const response = await fetch("https://api.adviceslip.com/advice");
+        const data = await response.json();
+        console.log(data);
+
+        adviceData.innerText = `"${data?.slip?.advice}"`;
+        idVal.innerText = `#${data?.slip?.id}`
+      } catch (error) {
+        console.error("Error while fetching advice:", error);
+        adviceData.innerText = "Error found"; 
+      }
 }
 
-getApiData();
-
-const adviceData = document.getElementById("advice-data");
+diceContainer.addEventListener("click", getApiData);
 
